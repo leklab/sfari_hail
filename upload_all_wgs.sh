53b1a4cca1c594e3bf912c1edcc8e813a0fabca2
monkollek <monkol.lek@yale.edu>
2020-07-29 16:02:24 +0000

    Additional edits to WGS upoad code

---
+++ upload_all_wgs.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm ~/hail_progress.txt
+
+c=20
+#for c in {1..22} X
+#do
+    d=`date`
+    echo "$d Processing: SPARK_WGS_1.chr${c}.JoinCalling.exome.ht" >> ~/hail_progress.txt
+    python submit.py --run-locally hail_scripts/populate_wgs_data.py \
+    --spark-home /home/ubuntu/bin/spark-2.4.3-bin-hadoop2.7 \
+    --cpu-limit 4 --driver-memory 16G --executor-memory 8G \
+    -i ~/data/hail_tables/wgs3/SPARK_WGS_1.chr${c}.JoinCalling.exome.ht
+#done
+
